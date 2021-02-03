a07819ce644ff85cd37603bc256e14b6818aa8e4
Yosuke Karasawa <903885+karakaram@users.noreply.github.com>
2021-02-03 16:47:35 +0900

    Fix 'ActiveAdmin::DatabaseHitDuringLoad' error when running 'rails db:create' command

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+  # Replace ActiveAdmin.routes(self) with below line. For details, see https://github.com/hzamani/acts_as_relation/issues/71
+  ActiveAdmin.routes(self) rescue ActiveAdmin::DatabaseHitDuringLoad
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
