aea9935c2f8b60e2b37455b739d86743c7fc0ee5
Yosuke Karasawa <903885+karakaram@users.noreply.github.com>
2021-02-05 18:29:46 +0900

    Implement feature to display the total amount of this month and last month to the dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,31 +2,48 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    # div class: "blank_slate_container", id: "dashboard_default_message" do
+    #   span class: "blank_slate" do
+    #     span I18n.t("active_admin.dashboard_welcome.welcome")
+    #     small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    #   end
+    # end
+
+    columns do
+      column do
+        panel "This Month Expenses" do
+          table_for Expense.joins(:expense_type).select('expenses.expense_type_id, sum(expenses.price) as expenses_price').where(paid_on: (Date.today.beginning_of_month)..(Date.today.beginning_of_month.next_month)).group(:expense_type_id).order("expenses_price DESC") do
+            column(:expense_type) { |expense| expense.expense_type.name }
+            column(:price) { |expense| expense.expenses_price }
+          end
+        end
+      end
+
+      column do
+        panel "This Month Incomes" do
+          table_for Income.joins(:income_type).select('incomes.income_type_id, sum(incomes.price) as incomes_price').where(earned_on: (Date.today.beginning_of_month)..(Date.today.beginning_of_month.next_month)).group(:income_type_id).order("incomes_price DESC") do
+            column(:income_type) { |income| income.income_type.name }
+            column(:price) { |income| income.incomes_price }
+          end
+        end
       end
     end
 
     columns do
       column do
-        panel "Recent Expenses" do
-          ul do
-            Expense.order(id: :desc).limit(20).map do |expense|
-              text = expense.paid_on.to_s + ' ' + expense.name.to_s + ' ' + expense.price.to_s
-              li link_to(text, admin_expense_path(expense))
-            end
+        panel "Last Month Expenses" do
+          table_for Expense.joins(:expense_type).select('expenses.expense_type_id, sum(expenses.price) as expenses_price').where(paid_on: (Date.today.prev_month.beginning_of_month)..(Date.today.beginning_of_month)).group(:expense_type_id).order("expenses_price DESC") do
+            column(:expense_type) { |expense| expense.expense_type.name }
+            column(:price) { |expense| expense.expenses_price }
           end
         end
       end
+
       column do
-        panel "Recent Income" do
-          ul do
-            Income.order(id: :desc).limit(20).map do |income|
-              text = income.earned_on.to_s + ' ' + income.name.to_s + ' ' + income.price.to_s
-              li link_to(text, admin_expense_path(income))
-            end
+        panel "Last Month Incomes" do
+          table_for Income.joins(:income_type).select('incomes.income_type_id, sum(incomes.price) as incomes_price').where(earned_on: (Date.today.prev_month.beginning_of_month)..(Date.today.beginning_of_month)).group(:income_type_id).order("incomes_price DESC") do
+            column(:income_type) { |income| income.income_type.name }
+            column(:price) { |income| income.incomes_price }
           end
         end
       end
