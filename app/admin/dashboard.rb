1ceb8897decd06bd4b592b612bb3a6ad2047ddc6
Yosuke Karasawa <903885+karakaram@users.noreply.github.com>
2021-02-06 22:57:33 +0900

    Add Price Summary Panel and remove the income list on Dashboard Page

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -5,6 +5,47 @@ ActiveAdmin.register_page "Dashboard" do
     columns do
       column do
         panel "This Month Expenses" do
+          table do
+            thead do
+              tr do
+                th do
+                  I18n.t("dashboard.price_type")
+                end
+                th do
+                  I18n.t("dashboard.price_summary")
+                end
+              end
+            end
+            tbody do
+              tr do
+                th do
+                  I18n.t("dashboard.expense")
+                end
+                td do
+                  link_to Expense.where(paid_on: (Date.today.beginning_of_month)..(Date.today.end_of_month)).sum(:price),
+                          admin_expenses_path(
+                            "q[paid_on_gteq]" => Date.today.beginning_of_month,
+                            "q[paid_on_lteq]" => Date.today.end_of_month,
+                            commit: "Filter"
+                          )
+                end
+              end
+              tr do
+                th do
+                  I18n.t("dashboard.income")
+                end
+                td do
+                  link_to Income.where(earned_on: (Date.today.beginning_of_month)..(Date.today.end_of_month)).sum(:price),
+                          admin_incomes_path(
+                            "q[earned_on_gteq]" => Date.today.beginning_of_month,
+                            "q[earned_on_lteq]" => Date.today.end_of_month,
+                            commit: "Filter"
+                          )
+                end
+              end
+            end
+          end
+
           table_for Expense.joins(:expense_type)
                            .select("expenses.expense_type_id, expense_types.name as expense_types_name, sum(expenses.price) as expenses_price")
                            .where(paid_on: (Date.today.beginning_of_month)..(Date.today.end_of_month))
@@ -22,27 +63,47 @@ ActiveAdmin.register_page "Dashboard" do
       end
 
       column do
-        panel "This Month Incomes" do
-          table_for Income.joins(:income_type)
-                          .select("incomes.income_type_id, income_types.name as income_types_name, sum(incomes.price) as incomes_price")
-                          .where(earned_on: (Date.today.beginning_of_month)..(Date.today.end_of_month))
-                          .group(:income_type_id)
-                          .order("incomes_price DESC") do
-            column(:income_type) { |income| income.income_types_name }
-            column(:price) { |income| link_to income.incomes_price, admin_incomes_path(
-              "q[income_type_id_eq]" => income.income_type_id,
-              "q[earned_on_gteq]" => Date.today.beginning_of_month,
-              "q[earned_on_lteq]" => Date.today.end_of_month,
-              commit: "Filter"
-            ) }
-          end
-        end
-      end
-    end
-
-    columns do
-      column do
         panel "Last Month Expenses" do
+          table do
+            thead do
+              tr do
+                th do
+                  I18n.t("dashboard.price_type")
+                end
+                th do
+                  I18n.t("dashboard.price_summary")
+                end
+              end
+            end
+            tbody do
+              tr do
+                th do
+                  I18n.t("dashboard.expense")
+                end
+                td do
+                  link_to Expense.where(paid_on: (Date.today.prev_month.beginning_of_month)..(Date.today.prev_month.end_of_month)).sum(:price),
+                          admin_expenses_path(
+                            "q[paid_on_gteq]" => Date.today.prev_month.beginning_of_month,
+                            "q[paid_on_lteq]" => Date.today.prev_month.end_of_month,
+                            commit: "Filter"
+                          )
+                end
+              end
+              tr do
+                th do
+                  I18n.t("dashboard.income")
+                end
+                td do
+                  link_to Income.where(earned_on: (Date.today.prev_month.beginning_of_month)..(Date.today.prev_month.end_of_month)).sum(:price),
+                          admin_incomes_path(
+                            "q[earned_on_gteq]" => Date.today.prev_month.beginning_of_month,
+                            "q[earned_on_lteq]" => Date.today.prev_month.end_of_month,
+                            commit: "Filter"
+                          )
+                end
+              end
+            end
+          end
           table_for Expense.joins(:expense_type)
                            .select("expenses.expense_type_id, expense_types.name as expense_types_name, sum(expenses.price) as expenses_price")
                            .where(paid_on: (Date.today.prev_month.beginning_of_month)..(Date.today.prev_month.end_of_month))
@@ -58,25 +119,6 @@ ActiveAdmin.register_page "Dashboard" do
           end
         end
       end
-
-      column do
-        panel "Last Month Incomes" do
-          table_for Income.joins(:income_type)
-                          .select('incomes.income_type_id, income_types.name as income_types_name, sum(incomes.price) as incomes_price')
-                          .where(earned_on: (Date.today.prev_month.beginning_of_month)..(Date.today.prev_month.end_of_month))
-                          .group(:income_type_id)
-                          .order("incomes_price DESC") do
-            column(:income_type) { |income| income.income_types_name }
-            column(:price) { |income| link_to income.incomes_price, admin_incomes_path(
-              "q[income_type_id_eq]" => income.income_type_id,
-              "q[earned_on_gteq]" => Date.today.prev_month.beginning_of_month,
-              "q[earned_on_lteq]" => Date.today.prev_month.end_of_month,
-              commit: "Filter"
-            ) }
-          end
-        end
-      end
     end
-
   end # content
 end
