0223a72076bd0fd78bf80e0225d9d6c36039db26
Yosuke Karasawa <903885+karakaram@users.noreply.github.com>
2021-02-07 14:29:48 +0900

    Add Price Summary List of spending two monthe ago on Dashboard Page

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,15 +4,15 @@ ActiveAdmin.register_page "Dashboard" do
   content title: proc { I18n.t("active_admin.dashboard") } do
     columns do
       column do
-        panel "This Month Expenses" do
+        panel "This Month" do
           table do
             thead do
               tr do
                 th do
-                  I18n.t("dashboard.price_type")
+                  I18n.t("dashboard.type")
                 end
                 th do
-                  I18n.t("dashboard.price_summary")
+                  I18n.t("dashboard.summary")
                 end
               end
             end
@@ -63,15 +63,15 @@ ActiveAdmin.register_page "Dashboard" do
       end
 
       column do
-        panel "Last Month Expenses" do
+        panel "Last Month" do
           table do
             thead do
               tr do
                 th do
-                  I18n.t("dashboard.price_type")
+                  I18n.t("dashboard.type")
                 end
                 th do
-                  I18n.t("dashboard.price_summary")
+                  I18n.t("dashboard.summary")
                 end
               end
             end
@@ -114,6 +114,64 @@ ActiveAdmin.register_page "Dashboard" do
               "q[expense_type_id_eq]" => expense.expense_type_id,
               "q[paid_on_gteq]" => Date.today.prev_month.beginning_of_month,
               "q[paid_on_lteq]" => Date.today.prev_month.end_of_month,
+              commit: "Filter"
+            ) }
+          end
+        end
+      end
+
+      column do
+        panel "Two month ago" do
+          table do
+            thead do
+              tr do
+                th do
+                  I18n.t("dashboard.type")
+                end
+                th do
+                  I18n.t("dashboard.summary")
+                end
+              end
+            end
+            tbody do
+              tr do
+                th do
+                  I18n.t("dashboard.expense")
+                end
+                td do
+                  link_to Expense.where(paid_on: (Date.today.ago(2.month).beginning_of_month)..(Date.today.ago(2.month).end_of_month)).sum(:price),
+                          admin_expenses_path(
+                            "q[paid_on_gteq]" => Date.today.ago(2.month).beginning_of_month,
+                            "q[paid_on_lteq]" => Date.today.ago(2.month).end_of_month,
+                            commit: "Filter"
+                          )
+                end
+              end
+              tr do
+                th do
+                  I18n.t("dashboard.income")
+                end
+                td do
+                  link_to Income.where(earned_on: (Date.today.ago(2.month).beginning_of_month)..(Date.today.ago(2.month).end_of_month)).sum(:price),
+                          admin_incomes_path(
+                            "q[earned_on_gteq]" => Date.today.ago(2.month).beginning_of_month,
+                            "q[earned_on_lteq]" => Date.today.ago(2.month).end_of_month,
+                            commit: "Filter"
+                          )
+                end
+              end
+            end
+          end
+          table_for Expense.joins(:expense_type)
+                           .select("expenses.expense_type_id, expense_types.name as expense_types_name, sum(expenses.price) as expenses_price")
+                           .where(paid_on: (Date.today.ago(2.month).beginning_of_month)..(Date.today.ago(2.month).end_of_month))
+                           .group(:expense_type_id)
+                           .order("expenses_price DESC") do
+            column(:expense_type) { |expense| expense.expense_types_name }
+            column(:price) { |expense| link_to expense.expenses_price, admin_expenses_path(
+              "q[expense_type_id_eq]" => expense.expense_type_id,
+              "q[paid_on_gteq]" => Date.today.ago(2.month).beginning_of_month,
+              "q[paid_on_lteq]" => Date.today.ago(2.month).end_of_month,
               commit: "Filter"
             ) }
           end
